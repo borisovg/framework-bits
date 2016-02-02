ffed67c6d604f369f198f055412a69d81dfeec9b
George Borisov <git@gir.me.uk>
2016-02-02 19:07:22 +0000

    Fix constructor arguments to match MySQL class

--- lib/Borisov/DB/SQLite.php
+++ lib/Borisov/DB/SQLite.php
@@ -9,13 +9,15 @@ use Borisov\Config;
 
 class SQLite extends Base
 {
-	public function __construct ($file = '', $opt = []) {
+	public function __construct ($type = 'ro', $opt = [], $file = false) {
 		if (!$file) {
 			if (!$file = Config::get('sqlite_file')) {
 				parent::error('Missing configuration');	
 			}
 		}
-		
+
+        //FIXME: R/O mode is not supported by PDO :-(
+
 		parent::__construct('sqlite:' . ROOT_PATH . $file, false, false, $opt);
 	}
 }
